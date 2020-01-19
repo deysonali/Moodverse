c05f8ea6056148610fdc297315c1e86e1501a983
addy999 <jude.addy999@gmail.com>
2020-01-19 03:29:25 +0000

    tweaks

--- Datasets/Negative_thoughts/reddit.py
+++ Datasets/Negative_thoughts/reddit.py
@@ -19,11 +19,11 @@ for sub in subs:
   print(sub)
   posts = download_top(sub, THREAD_LIMIT)
   subs[sub] = posts
-
+  print("Found {} posts".format(len(posts)))
 print("Saving")
 
-f = open("Datasets/Negative_thoughts/reddit_posts.txt", "w" )
-[[f.write(text + "/n") for text in subs[sub]] for sub in subs]
+f = open(r"reddit_posts.txt", "w" )
+[[f.write(text + "\n") for text in subs[sub]] for sub in subs]
 f.close()
 
 print("Saved")
