20ce9aa1cf05bb56991ec0c408db69b13be127d1
addy999 <addy@loftorbital.com>
2020-01-19 05:09:33 -0500

    make bot.py

---
+++ Server/back/bot.py
@@ -0,0 +1,20 @@
+import sys
+
+class Bot:
+    
+    def __init__(self, inital_message = None):
+        
+        self.inital_message = inital_message
+        self.get_inital_message()
+        
+    def send(message):
+        sys.stdout.write(str(message))
+    
+    def get_inital_message(self):
+        
+        if not inital_message:
+            self.send("NONE")
+
+        # Generate categorical insight response
+        
+        
