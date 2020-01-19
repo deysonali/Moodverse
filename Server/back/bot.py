7e4b2fd58b75695ed2f681c863ec76b1c16c566f
addy999 <addy@loftorbital.com>
2020-01-19 09:01:24 -0500

    exploration stage added

--- Server/back/bot.py
+++ Server/back/bot.py
@@ -8,21 +8,33 @@ class Bot:
     def __init__(self, 
                  sympathetic_responses = BASE_REPLIES["sympathetic"],
                  clarification_responses = (BASE_REPLIES["clarify"], BASE_REPLIES["clarify_entity"]),
+                 reframe_responses = BASE_REPLIES["reframing"],
+                 cog_responses = BASE_REPLIES["cog_dist"],
                  flag_detect = flag_detect,
-                 exploration_max = 3,
+                 explorations_left = 3,
+                 stage_12_threshold = 2/3,
+                 stage_23_threshold = 1/3,
                  ):
         
         self.sympatheticResponse = randomResponses(sympathetic_responses)
-        self.cogResponse = cogDist(BASE_REPLIES["cog_dist"])
         self.clarificationResponse = clarificationResponse(clarification_responses[0], clarification_responses[1])
+        self.reframeResponse = reFrame(reframe_responses)
+        self.cogResponse = cogDist(cog_responses)
         
         self.flag_check = flag_detect
-        self.seq = Sequence(
+        self.seq1 = Sequence(
             self.get_initial_message,
             self.follow_exploration,
             )
+        # self.seq2 = CyclicSequence(
+        #     self.get_stage1_response,
+        #     self.get_stage2_response,
+        #     self.get_stage3_response
+        #     )
         self.profile = Profile()
-        self.exploration_max = exploration_max
+        self.explorations_left = explorations_left
+        self.stage_12_threshold = stage_12_threshold
+        self.stage_23_threshold = stage_23_threshold
 
     def capture(self, message):
         
@@ -40,6 +52,15 @@ class Bot:
             if word in message.lower():
                 return True
     
+    def follow_exploration(self, message):
+        
+        recent_score = self.profile.sl_scores[-1]
+        
+        if self.explorations_left <= 0:
+            self.exit(recent_score)
+            
+        if     
+    
     def get_response(self, message):
         
         self.capture(message)
@@ -52,7 +73,7 @@ class Bot:
             self.exit(1)
             return
         
-        response = self.seq.next()(message)
+        response = self.seq1.next()(message)
     
     def send(message):
         return format_reply(str(message))
@@ -85,13 +106,24 @@ class Bot:
         )
 
     def get_stage1_response(self, message):
-        
-        ent = fine_worst_entity(message)
-        
+
         self.send_sequential_message(
             self.sympatheticResponse.generate(),
-            self.clarificationResponse.generate(entity=ent),
+            self.clarificationResponse.generate(message),
         )
+     
+    def get_stage2_response(self, message):
+
+        self.send_sequential_message(
+            self.reframeResponse.generate(message),
+        )
+     
+    def get_stage3_response(self, message):
+
+        self.send_sequential_message(
+            self.proudResponse.generate(),
+        )
+        self.exit(self.sl_scores[-1])
         
 class Profile:
     
@@ -111,6 +143,7 @@ class Sequence:
     
     def __init__(self, methods):
         self.methods = methods
+        self.untouched = True
         self.last = self.methods[0]
 
     def _get_pos(self, method):
@@ -118,9 +151,33 @@ class Sequence:
     
     def next(self):
         i = self._get_pos(self.last)
-        if i+1 < len(self.methods)-1:
-            self.last = self.methods[i+1]
-            return self.methods[i+1]
+        if self.untouched:
+            self.untouched = False
+            return self.methods[i]
+        
+        next_i = i+1
+        if next_i <= len(self.methods)-1:
+            self.last = self.methods[next_i]
+            return self.methods[next_i]
+
+class CyclicSequence:
+    
+    def __init__(self, methods):
+        self.methods = methods
+        self.last = self.methods[0]
+
+    def _get_pos(self, method):
+        return self.methods.index(method)
+    
+    def next(self):
+        c = self._get_pos(self.last)
+        next_c = c+1
+        
+        if next_c > len(self.methods)-1:
+            next_c = next_c % len(self.methods)
+            
+        self.last = self.methods[next_c]
+        return self.methods[next_c]
         
 def flag_detect(message):
     if "sadness" in emergency_model.predict(message)[0]:
