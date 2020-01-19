6f3b70886ff3d2df08ea5942394f12cc662da424
wurun <run.wu@mail.utoronto.ca>
2020-01-19 08:00:36 -0500

    Finished insights, chatbot interface.

--- Server/front/src/app/talk/talk.component.ts
+++ Server/front/src/app/talk/talk.component.ts
@@ -3,8 +3,9 @@ import {ChatDialogComponent} from './chat/chat-dialog/chat-dialog.component';
 
 @Component({
   selector: 'app-talk',
-  templateUrl: './talk.component.html',
-  styleUrls: ['./talk.component.css']
+  templateUrl: './index.html',
+  styleUrls: ['../../assets/chat/css/styles.css']
+ // styleUrls: ['C:\\Users\\RainWu\\IdeaProjects\\Moodverse\\Server\\front\\src\\assets\\chat\\css\\styles.css']
 })
 export class TalkComponent implements OnInit {
 
