6ad3d8028ff70de46d2cd0ec0d1aa624c782efa6
addy999 <jude.addy999@gmail.com>
2020-01-19 04:59:32 +0000

    make lstm script

---
+++ Training/LSTM.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+
+# In[2]:
+
+
+#Loading the emotions dataset
+dataset = pd.read_csv("../Datasets/Positive_thoughts/emotion.data")
+dataset = pd.concat([dataset[dataset.emotions == "joy"], dataset[dataset.emotions == "sadness"]])
+dataset = dataset.drop(dataset.columns[0], 1)
+
+
+# In[4]:
+
+
+# Loading reddit dataset
+r_dataset = pd.read_csv("../Datasets/Negative_thoughts/reddit_posts.txt", sep="\n", header=None)
+r_dataset.columns = ["text"]
+r_dataset["emotions"] = ["sadness" for i in range(len(r_dataset))]
+
+
+# In[5]:
+
+
+dataset = pd.concat([dataset, r_dataset])
+
+
+# In[7]:
+
+
+dataset.emotions.value_counts().plot.bar()
+
+
+# In[8]:
+
+
+input_sentences = [text.split(" ") for text in dataset["text"].values.tolist()]
+labels = dataset["emotions"].values.tolist()
+
+
+# In[9]:
+
+
+word2id = dict()
+label2id = dict()
+
+max_words = 0 # maximum number of words in a sentence
+
+# Construction of word2id dict
+for sentence in input_sentences:
+    for word in sentence:
+        # Add words to word2id dict if not exist
+        if word not in word2id:
+            word2id[word] = len(word2id)
+    # If length of the sentence is greater than max_words, update max_words
+    if len(sentence) > max_words:
+        max_words = len(sentence)
+    
+# Construction of label2id and id2label dicts
+label2id = {l: i for i, l in enumerate(set(labels))}
+id2label = {v: k for k, v in label2id.items()}
+id2label
+
+
+# In[10]:
+
+
+import keras
+
+# Encode input words and labels
+X = [[word2id[word] for word in sentence] for sentence in input_sentences]
+Y = [label2id[label] for label in labels]
+
+# Apply Padding to X
+from keras.preprocessing.sequence import pad_sequences
+X = pad_sequences(X, max_words)
+
+# Convert Y to numpy array
+Y = keras.utils.to_categorical(Y, num_classes=len(label2id), dtype='float32')
+
+# Print shapes
+print("Shape of X: {}".format(X.shape))
+print("Shape of Y: {}".format(Y.shape))
+
+
+# ## LSTM
+
+# In[11]:
+
+
+embedding_dim = 100 # The dimension of word embeddings
+
+# Define input tensor
+sequence_input = keras.Input(shape=(max_words,), dtype='int32')
+
+# Word embedding layer
+embedded_inputs =keras.layers.Embedding(len(word2id) + 1,
+                                        embedding_dim,
+                                        input_length=max_words)(sequence_input)
+
+# Apply dropout to prevent overfitting
+embedded_inputs = keras.layers.Dropout(0.2)(embedded_inputs)
+
+# Apply Bidirectional LSTM over embedded inputs
+lstm_outs = keras.layers.wrappers.Bidirectional(
+    keras.layers.LSTM(embedding_dim, return_sequences=True)
+)(embedded_inputs)
+
+# Apply dropout to LSTM outputs to prevent overfitting
+lstm_outs = keras.layers.Dropout(0.2)(lstm_outs)
+
+# Attention Mechanism - Generate attention vectors
+input_dim = int(lstm_outs.shape[2])
+permuted_inputs = keras.layers.Permute((2, 1))(lstm_outs)
+attention_vector = keras.layers.TimeDistributed(keras.layers.Dense(1))(lstm_outs)
+attention_vector = keras.layers.Reshape((max_words,))(attention_vector)
+attention_vector = keras.layers.Activation('softmax', name='attention_vec')(attention_vector)
+attention_output = keras.layers.Dot(axes=1)([lstm_outs, attention_vector])
+
+# Last layer: fully connected with softmax activation
+fc = keras.layers.Dense(embedding_dim, activation='relu')(attention_output)
+output = keras.layers.Dense(len(label2id), activation='softmax')(fc)
+
+# Finally building model
+model = keras.Model(inputs=[sequence_input], outputs=output)
+model.compile(loss="categorical_crossentropy", metrics=["accuracy"], optimizer='adam')
+
+# Print model summary
+model.summary()
+
+
+# In[ ]:
+
+
+# Train model 10 iterations
+model.fit(X, Y, epochs=2, batch_size=64, validation_split=0.1, shuffle=True)
+model.save("./msg_flag_epochs=2, batch_size=64, validation_split=0.1,")
+
